a277e5d3fbf87d63e852250540f8e5817fe74a68
byyMac <471073043@qq.com>
2016-09-29 20:21:08 +0800

    init my template

---
+++ fslib/Component1.fs
@@ -0,0 +1,11 @@
+﻿namespace fslib
+
+open UnityEngine
+
+type Class1() = 
+    member this.X = "F#"
+
+type MyBehaviour() =
+    inherit MonoBehaviour()
+    member this.x = 1
+    member this.Start() = Debug.Log "hello from F#"
